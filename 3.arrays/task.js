2e4563b5e30b8413f511b201193d8539c873d256
E.Rodionova <makantero>
2022-12-10 14:27:07 +0300

    update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
-  return result; // boolean
+  result = (arr1.length == arr2.length) && arr1.every((element, index) => element === arr2[index]);
+  return result; 
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter((el) => el > 0).filter((element) => element % 3 === 0).map((element) => element * 10)
 
-  return resultArr; // array
+  return resultArr;
 }
