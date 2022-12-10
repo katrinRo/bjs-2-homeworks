595753502cd4e59be7720bf4190b6057f2867557
E.Rodionova <makantero>
2022-12-10 21:16:11 +0300

    update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,45 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
+}
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+    }
 }
 
-// ваш код для остальных методов
+Student.prototype.addMarks = function (...mark) {
+   if(this.marks === undefined){ 
+    this.marks = mark;
+    } else {
+      this.marks.push(...mark);
+    }
+}
+
+Student.prototype.getAverage = function () {
+  if (this.marks.length === 0) {
+    return 0;
+  } else {
+    let sum = 0;
+    let count = 0;
+    this.marks.map((element) => {
+      count += 1;
+      sum += element;
+    })
+    return sum/count;
+  }
+
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
