a7cd32703746717ec7955fc85794ee7741e6cd0c
E.Rodionova <makantero>
2023-03-10 19:48:46 +0300

    update task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,42 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+    let cache = [];
+
+    function wrapper(...args) {
+        const hash = args.join(',');
+        let objectInCache = cache.find((item) => item === hash);
+        if (objectInCache) {
+            objectInCache = func.call(this, ...args);
+            console.log("Из кэша: " + objectInCache);
+            return "Из кэша: " + objectInCache;
+        }
+        let result = func(...args);
+        cache.push(hash) ;
+        if (cache.length > 5) {
+            cache.shift();
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
+
+    return wrapper;
 }
 
 
+
 function debounceDecoratorNew(func) {
-  // Ваш код
+    let timeoutId = null;
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    return function wrapper(...args) {
+        if (timeoutId === null) {
+            wrapper.count++;
+            func(...args);
+        }
+        clearTimeout(timeoutId);
+
+        timeoutId = setTimeout(() => {
+            wrapper.count++;
+            func(...args);
+        }, delay)
+    }
 }
